0130f0ffc930de21624c13faf2b7e81fea7c07d6
Jenessa <jenessaw31@gmail.com>
2016-11-02 16:03:37 -0500

    updated assignment with walkthrough answers

--- src/index.js
+++ src/index.js
@@ -4,71 +4,113 @@
 // write your own forEach() function that takes an array and a function
 // ----------------------------
 
-function forEach(array, callback){
+function forEach(array, callback) {
     // YOUR CODE HERE
+    for (var i = 0; i < array.length; i++) {
+        callback(array[i], i, array);
+    }
 }
-
 // tests
 // ---
 var total = 1
-forEach([1, 2, 3, 4], function(a){ total *= a; })
+forEach([
+    1, 2, 3, 4
+], function(a) {
+    total = total * a;
+})
 console.assert(total === 24)
-
-// ----------------------------
-// using forEach() from above, write your own reduce()
-// that takes an array and a function
-// ----------------------------
-
-function reduce(array, callback){
+//
+// // ----------------------------
+// // using forEach() from above, write your own reduce()
+// // that takes an array and a function
+// // ----------------------------
+//
+function reduce(array, callback) {
     // YOUR CODE HERE
+  var accumulator;
+  forEach(array, function(value, i, ogArray){
+    if (i===0) {
+      accumulator = value;
+    } else {
+      accumulator = callback(accumulator, value);
+    }
+  });
+  return accumulator;
 }
 
 // tests
 // ---
-console.assert(
-    reduce([1, 2, 3, 4], function(a, v){ return a*v }) === 24
-)
-
-// ----------------------------
-// using forEach() from above, write your own map()
-// that takes an array and a function
-// ----------------------------
-
-function map(array, callback){
+console.assert(reduce([
+    1, 2, 3, 4
+], function(a, v) {
+    return a * v
+}) === 24)
+//
+// // ----------------------------
+// // using forEach() from above, write your own map()
+// // that takes an array and a function
+// // ----------------------------
+//
+function map(array, callback) {
     // YOUR CODE HERE
+    var mappedArray = [];
+    forEach(array, function(value, i, ogArray) {
+      mappedArray.push(callback(value, i, ogArray));
+    });
+
+    return mappedArray;
 }
 
 // tests
 // ---
-var squares = map([1, 2, 3, 4], function(v){ return v*v })
+var squares = map([
+    1, 2, 3, 4
+], function(v) {
+    return v * v
+})
 console.assert(squares[0] === 1)
 console.assert(squares[1] === 4)
 console.assert(squares[2] === 9)
 console.assert(squares[3] === 16)
-
-// ----------------------------
-// using reduce() from above, write your own filter()
-// that takes an array and a function
-// ----------------------------
-
-function filter(array, callback){
+//
+// // ----------------------------
+// // using reduce() from above, write your own filter()
+// // that takes an array and a function
+// // ----------------------------
+//
+function filter(array, callback) {
     // YOUR CODE HERE
+  var filteredArray = [];
+  reduce(array, function (a, value, i, ogArray) {
+    var resultOfCallback = callback(value, i, ogArray);
+    if (resultOfCallback) {
+      filteredArray.push(value);
+    };
+  });
+  return filteredArray;
 }
 
 // tests
 // ---
-var evens = filter([1, 2, 3, 4], function(v){ return v%2 === 0 })
+var evens = filter([
+    1, 2, 3, 4
+], function(v) {
+    return v % 2 === 0
+})
 console.assert(evens[0] === 2)
 console.assert(evens[1] === 4)
-
 
 // ----------------------------
 // using reduce() from above, write your own sum()
 // that adds up all arguments to sum (note: variadic behavior)
 // ----------------------------
 
-function sum(){
+function sum() {
     // YOUR CODE HERE
+    var arguments = [].slice.call(arguments);
+    return reduce(arguments, function(a, value) {
+      return a + value;
+    });
 }
 
 // tests
@@ -81,15 +123,23 @@ console.assert(sum(1, 2, 3, 4, 5) === 15)
 // using Array.sort(), sort the following array
 // of people by name
 // ----------------------------
-
+//
 var names = [
-    {name:"Matt", alma_mater:"Univ of Texas - Austin"},
-    {name:"Brian", alma_mater:"Texas A&M"},
-    {name:"Jesse", alma_mater:"Univ of Texas - Austin"}
+    {
+        name: "Matt",
+        alma_mater: "Univ of Texas - Austin"
+    }, {
+        name: "Brian",
+        alma_mater: "Texas A&M"
+    }, {
+        name: "Jesse",
+        alma_mater: "Univ of Texas - Austin"
+    }
 ]
 
-names.sort(function(a, b){
+names.sort(function(a, b) {
     // YOUR CODE HERE
+    return a.name > b.name;
 })
 
 // tests
@@ -97,32 +147,40 @@ names.sort(function(a, b){
 console.assert(names[0].name === "Brian")
 console.assert(names[1].name === "Jesse")
 console.assert(names[2].name === "Matt")
-
-// ----------------------------
-// Using Array.map(), Array.filter(), and Array.sort() on the
-// array below:
-// - filter for customers whose first-names start with 'J',
-// - map to their fullnames,
-// - and then sort the items alphabetically by fullname
-// ----------------------------
-
+//
+// // ----------------------------
+// // Using Array.map(), Array.filter(), and Array.sort() on the
+// // array below:
+// // - filter for customers whose first-names start with 'J',
+// // - map to their fullnames,
+// // - and then sort the items alphabetically by fullname
+// // ----------------------------
+//
 var customers = [
-    { first: 'Joe', last: 'Blogs'},
-    { first: 'John', last: 'Smith'},
-    { first: 'Dave', last: 'Jones'},
-    { first: 'Jack', last: 'White'}
+    {
+        first: 'Joe',
+        last: 'Blogs'
+    }, {
+        first: 'John',
+        last: 'Smith'
+    }, {
+        first: 'Dave',
+        last: 'Jones'
+    }, {
+        first: 'Jack',
+        last: 'White'
+    }
 ]
 
-var results = customers
-    .filter(function(){
-        // YOUR CODE HERE
-    })
-    .map(function(){
-        // YOUR CODE HERE
-    })
-    .sort(function(){
-        // YOUR CODE HERE
-    })
+var results = customers.filter(function(value) {
+    return value.first.charAt(0) === 'J'
+}).map(function(value, i, ogArray) {
+    var newCustomer = value;
+    newCustomer.fullname = value.first + " " + value.last;
+    return newCustomer
+}).sort(function(a, b) {
+    return a.fullname > b.fullname;
+})
 
 // tests
 // ---
